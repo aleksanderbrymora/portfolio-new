0a34e1ae5b36d5c66068d6bac154e16698c3980d
aleksanderbrymora <aleksander.australia@gmail.com>
2020-01-16 02:33:47 +1100

    titles are now displaying with indication which one is active

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+
 import Layout from '../components/Layout';
 import Head from '../components/Head';
 import style from '../styles/projects.module.sass';
 
 export default () => {
+  const data = useStaticQuery(
+    graphql`
+      query projects {
+        allContentfulProject(sort: { fields: created, order: DESC }) {
+          edges {
+            node {
+              createdAt(formatString: "MMMM YYYY")
+              description {
+                json
+              }
+              hostedAt
+              github
+              slug
+              title
+              projectShowcase {
+                file {
+                  url
+                }
+              }
+              featured
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const featured = data.allContentfulProject.edges.find(
+    edge => edge.node.featured
+  );
+  const all = data.allContentfulProject.edges;
+  const [active, setActive] = useState(featured.node);
+
   return (
     <Layout>
-      <div className={style.height}>
-        <Head title={'Projects'} />
-        {}
-        <h1>Hii</h1>
+      <Head title={'Projects'} />
+      <div className={style.projects}>
+        <div>
+          {all.map(edge => (
+            <div key={edge.node.slug} className={style.titles}>
+              <p className={style.indicator}>
+                {active.slug === edge.node.slug ? '♦' : ''}
+              </p>
+              <p
+                className={
+                  style.text +
+                  (active.slug === edge.node.slug ? style.activeTitle : '')
+                }
+              >
+                {edge.node.title}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className={style.project}>
+          <h1>{active.title}</h1>
+        </div>
       </div>
     </Layout>
   );
