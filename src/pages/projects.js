7f61cf9affa5c8b7d5914c4298e30c3bf86e8216
aleksanderbrymora <aleksander.australia@gmail.com>
2020-01-16 04:17:40 +1100

    managed to display each project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/Layout';
 import Head from '../components/Head';
 import style from '../styles/projects.module.sass';
+import github from '../img/github.svg';
+import website from '../img/website.svg';
 
 export default () => {
   const data = useStaticQuery(
@@ -39,13 +42,24 @@ export default () => {
   const all = data.allContentfulProject.edges;
   const [active, setActive] = useState(featured.node);
 
+  const handleProjectChange = slug => {
+    const newProject = all.find(edge => edge.node.slug === slug);
+    console.log({ newProject });
+    setActive(newProject.node);
+    console.log({ active });
+  };
+
   return (
     <Layout>
       <Head title={'Projects'} />
       <div className={style.projects}>
         <div>
           {all.map(edge => (
-            <div key={edge.node.slug} className={style.titles}>
+            <div
+              key={edge.node.slug}
+              className={style.titles}
+              onClick={() => handleProjectChange(edge.node.slug)}
+            >
               <p className={style.indicator}>
                 {active.slug === edge.node.slug ? '♦' : ''}
               </p>
@@ -60,9 +74,28 @@ export default () => {
             </div>
           ))}
         </div>
-        <div className={style.project}>
-          <h1>{active.title}</h1>
-        </div>
+        {active.description ? (
+          <div className={style.project}>
+            <h1>{active.title}</h1>
+            <div className={style.dateLinks}>
+              <p>{active.createdAt}</p>
+              <a href={active.hostedAt}>
+                <img src={website} alt="github link" />
+              </a>
+              <a href={active.github}>
+                <img src={github} alt="" />
+              </a>
+            </div>
+            <div className={style.description}>
+              {documentToReactComponents(active.description.json)}
+            </div>
+            <img
+              className={style.showcase}
+              src={`https://${active.projectShowcase.file.url}`}
+              alt="app showcase"
+            />
+          </div>
+        ) : null}
       </div>
     </Layout>
   );
--- src/templates/project.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Layout from '../components/Layout';
-
-export default props => {
-  return <Layout></Layout>;
-};
