3922e7ef7f10ef86657eaf8598a060b7a213829e
aleksanderbrymora <aleksander.australia@gmail.com>
2020-01-22 20:04:41 +1100

    pulling

--- src/pages/other.js
+++ src/pages/other.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Head from '../components/Head';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export default () => (
-  <Layout>
-    <Head title={'Other'} />
-    <h1>Hii</h1>
-  </Layout>
-);
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulHobby {
+        description {
+          description
+        }
+        title
+        url
+        embed {
+          embed
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+
+  return (
+    <Layout>
+      <Head title={'Other'} />
+      <h1>Hii</h1>
+    </Layout>
+  );
+};
